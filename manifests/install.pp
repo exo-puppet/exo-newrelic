4387b0e33a5a338d7eb6def21c070395117bd4a0
Frédéric DROUET <fdrouet@exoplatform.com>
2016-05-21 11:12:12 +0200

    Use official apt module instead of eXo repo module

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,27 +2,28 @@ class newrelic::install {
   case $::operatingsystem {
     /(Ubuntu)/ : {
       # Add the NewRelic .deb repository
-      repo::define { 'newrelic-repo':
-        file_name    => 'newrelic',
-        url          => 'http://apt.newrelic.com/debian/',
-        distribution => 'newrelic',
-        sections     => ['non-free'],
-        source       => false,
-        key          => '548C16BF',
-        key_server   => 'keyserver.ubuntu.com',
-        notify       => Exec['repo-update'],
-      } ->
+      apt::source { 'newrelic':
+        location => 'http://apt.newrelic.com/debian/',
+        release  => 'newrelic',
+        repos    => 'non-free',
+        key      => {
+          'id'     => 'B60A3EC9BC013B9C23790EC8B31B29E5548C16BF',
+          'server' => 'keyserver.ubuntu.com',
+        },
+        include  => {
+          'src' => false,
+          'deb' => true,
+        },
+      }
       # The package to install
-      package { 'newrelic-sysmond':
-        ensure  => $newrelic::present ? {
+      ensure_packages ( 'newrelic-sysmond', {
+        'ensure'  => $newrelic::present ? {
           true    => present,
           default => purged,
         },
-        name    => $newrelic::params::package_name,
-        require => [
-          Repo::Define['newrelic-repo'],
-          Exec['repo-update']],
-      }
+        'name'    => $newrelic::params::package_name,
+        'require' => [Apt::Source['newrelic'],Class['apt::update']],
+      } )
     }
     default    : {
       fail("The ${module_name} module is not supported on ${::operatingsystem}")
